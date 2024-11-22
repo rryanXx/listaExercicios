5cd0a78943aaf731c7a46caa250bf651dac8621f
Xandrim17 <ryanevil77@gmail.com>
2024-11-21 21:12:31 -0300

    exercicios foreach

---
+++ foreach/foreach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listaamigo = { "ryan","fer","maria","didi" };
+
+            Console.WriteLine("os nome dos amigos sao");
+            foreach(string nome_amigos in listaamigo )
+            {
+                Console.WriteLine(nome_amigos);
+
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
